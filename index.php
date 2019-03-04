c8c2a5998f941c181e2d57775e3a2ba772ad32ec
sunjiaqiang <13913000712@163.com>
2019-03-04 16:46:16 +0800

    commit

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ ini_set("display_errors", "On");
 error_reporting(E_ALL);
 define('APP_PATH', realpath(dirname(__FILE__).'/../'));
 echo 'dddd';
+echo 'we';
 $app = new Yaf_Application(APP_PATH."/conf/application.ini");
 //echo '<pre>';
 //print_r($app);
